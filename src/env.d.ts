521b88d60cd437bab5e6604f28bd36cba4f6b382
Omran Jamal <o.jamal97@gmail.com>
2023-10-10 15:54:23 +0600

    messing with KV

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,14 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+
+type ENV = {
+  MAIN_KV: KVNamespace;
+};
+
+type Runtime = import("@astrojs/cloudflare").AdvancedRuntime<ENV>;
+
+declare namespace App {
+  interface Locals extends Runtime {}
+}
