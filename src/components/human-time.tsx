bb305f74ab9e4afb10c14bfb270bddcf05960af1
Omran Jamal <o.jamal97@gmail.com>
2024-02-17 01:11:36 +0600

    rendering time tooltip on the client-side only to be reader-aware

--- src/components/human-time.tsx
+++ src/components/human-time.tsx
@@ -1,13 +1,24 @@
+import { useEffect, useState } from "react";
 import { format } from "timeago.js";
 
 export function HumanTime({ time }: { time: Date }) {
+  const [renderTime, setRenderTime] = useState(false);
+
+  useEffect(() => {
+    setRenderTime(true);
+  }, [setRenderTime]);
+
   return (
     <span
-      title={time.toLocaleString("en-gb", {
-        dateStyle: "full",
-        timeStyle: "full",
-        hour12: true,
-      })}
+      title={
+        renderTime
+          ? time.toLocaleString("en-gb", {
+              dateStyle: "full",
+              timeStyle: "full",
+              hour12: true,
+            })
+          : ""
+      }
     >
       {format(time, "en_GB")}
     </span>
